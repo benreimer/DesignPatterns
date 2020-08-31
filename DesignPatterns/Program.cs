411111e74d98cd9d68628f9aa852c8da6b746e8b
Reimer,Ben <Ben.Reimer@Cerner.com>
2020-08-30 23:50:21 -0400

    create new main project with one class library. Each design pattern will be created as a class library

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Intro;
+
+
+namespace DesignPatterns
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            Program p = new Program();
+            p.Run();
+
+            Intro.Class1 newClass = new Class1();
+            Console.WriteLine(newClass.Print());
+
+            p.Exit();
+
+
+
+
+            
+
+        }
+
+
+        private void Run()
+        {
+            Console.WriteLine("program is running....");
+            
+        }
+
+        private void Exit()
+        {
+            Console.WriteLine("Exiting.....press any key to exit");
+            Console.ReadLine();
+
+        }
+    }
+}
